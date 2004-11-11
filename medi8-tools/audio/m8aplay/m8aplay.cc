d472db5aefa96bbb54940d2ba8c0364e2ca57453
green <green>
2004-11-11 16:40:58 +0000

    Introduce osc usage.

--- medi8-tools/audio/m8aplay/m8aplay.cc
+++ medi8-tools/audio/m8aplay/m8aplay.cc
@@ -23,16 +23,72 @@
 
 #include <stdlib.h>
 #include <unistd.h>
-#include <SDL/SDL.h>
+#include <lo/lo.h>
 
+// Forward declarations of OSC message handlers.
+static int
+generic_handler (const char *path, const char *types, lo_arg **argv, 
+                 int argc, void *data, void *user_data);
+
+static int
+volume_handler (const char *path, const char *types, lo_arg **argv, 
+                int argc, void *data, void *user_data);                
+                
+// The OSC error handler.
+static void
+error (int num, const char *msg, const char *path)
+{
+	log_error ("liblo server error %d in path %s: %s", num, path, msg);
+}
+                      
 int
 main (int argc, char *argv[])
 {
 	// FIXME do something smarter with log data.
 	init_logger (MEDI8_LOG_DEBUG, "/tmp/m8aplay.log");
 	
+	// Start a new server on port 5333
+	// FIXME read the port from the cmd line.
+  lo_server_thread st = lo_server_thread_new ("5333", error);
+                                                                                
+  // add method that will match any path and args.
+  lo_server_thread_add_method (st, NULL, NULL, generic_handler, NULL);
+                                                                                
+  // add the volume handler.
+  lo_server_thread_add_method (st, "/medi8/audio/volume", 
+           	                   "i", volume_handler, NULL);
+
+  // Start the OSC server thread.
+  lo_server_thread_start (st);
+	
 	while (1)
 	{
 		sleep (100);
 	}
+}
+
+int
+generic_handler (const char *path, const char *types, lo_arg **argv,
+                 int argc, void *data, void *user_data)
+{
+  int i;
+                                                                                
+  log_debug ("path: <%s>\n", path);
+  for (i=0; i<argc; i++)
+  {
+    log_debug ("\targ %d '%c' ", i, types[i]);
+    lo_arg_pp ((lo_type) types[i], argv[i]);
+  }
+              
+  // Returning 1 means that we didn't handle the message, and
+  // the server should try other methods.                                                                            
+  return 1;
+}
+                                                                                
+int
+volume_handler (const char *path, const char *types, lo_arg **argv, 
+                int argc, void *data, void *user_data)
+{
+  log_debug ("setting volume to %d\n", argv[0]->i);
+  return 0;
 }
