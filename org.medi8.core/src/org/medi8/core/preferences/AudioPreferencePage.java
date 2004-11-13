22f799456894ccb4a304a022a3b75405bd4ab542
green <green>
2004-11-13 07:08:35 +0000

    Add ComboFieldEditor. Add preferences for audio output.

--- org.medi8.core/src/org/medi8/core/preferences/AudioPreferencePage.java
+++ org.medi8.core/src/org/medi8/core/preferences/AudioPreferencePage.java
@@ -1,9 +1,13 @@
 package org.medi8.core.preferences;
 
+import java.util.Enumeration;
+import java.util.Properties;
+
 import org.eclipse.jface.preference.*;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.IWorkbench;
 import org.medi8.core.CorePlugin;
+import org.medi8.internal.core.ui.ComboFieldEditor;
 import org.eclipse.jface.preference.IPreferenceStore;
 
 /**
@@ -25,7 +29,9 @@ public class AudioPreferencePage
 	extends FieldEditorPreferencePage
 	implements IWorkbenchPreferencePage {
 	public static final String P_BUFFER = "bufferPreference";
-
+	public static final String P_LOUT = "loutPreference";
+	public static final String P_ROUT = "routPreference";
+	
 	public AudioPreferencePage() {
 		super(GRID);
 		setPreferenceStore(CorePlugin.getDefault().getPreferenceStore());
@@ -48,11 +54,44 @@ public class AudioPreferencePage
  */
 
 	public void createFieldEditors() {
+	  
 	  IntegerFieldEditor ife =
 	    new IntegerFieldEditor (P_BUFFER, "Output &buffer size:", getFieldEditorParent());
 	  ife.setValidRange (1024, 8192);
 	  ife.setErrorMessage ("Buffer size must be between 1024 and 8192");
 		addField(ife);
+		
+		Properties prop = new Properties ();
+		try {
+			Process p = Runtime.getRuntime().exec(new String[] {
+					System.getProperty ("medi8.workspace") 
+						+ "/medi8-tools/audio/m8ainfo/m8ainfo" });
+
+			prop.load(p.getInputStream());
+			p.waitFor();
+		} catch (Exception _) {
+		  // Do nothing.
+		}
+		System.out.println ("read " + prop.size() + " properties");
+		String choices[][] = new String[prop.size()][2];
+		Enumeration e = prop.propertyNames();
+		int i = 0;
+		while (e.hasMoreElements ())
+		  {
+		  	String value = prop.getProperty ((String) e.nextElement());
+		  	choices[i][0] = choices[i][1] = value;
+		  	i++;
+		  }
+		  	           
+		ComboFieldEditor cfe =
+		  new ComboFieldEditor (P_LOUT, "Output jack, &left:", 
+		                        choices, getFieldEditorParent());
+		addField (cfe);
+		
+		cfe =
+		  new ComboFieldEditor (P_ROUT, "Output jack, &right:", 
+		                        choices, getFieldEditorParent());
+		addField (cfe);
 	}
 	
 	public void init(IWorkbench workbench) {
